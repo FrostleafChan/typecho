13f8a96aae67fda15a3a791a0d98c7adbac20be3
Elvin Starry <ltdoge@ltdoge.top>
2023-06-11 14:20:54 +0800

    sitedesc

---
+++ usr/themes/nexmoe/layout/_widget/sitedesc.php
@@ -0,0 +1,6 @@
+<div class="nexmoe-widget-wrap">
+    <h3 class="nexmoe-widget-title"><i class="mdui-icon material-icons">&#xe42d;</i>霜叶小屋</h3>
+    <div class="nexmoe-widget">
+        <p>“轻击键盘，静候回音。”</p>
+    </div>
+</div>
