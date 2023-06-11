2b47a734b2c01b8a1f66b77dbc9a9ba9110f8567
Elvin Starry <ltdoge@ltdoge.top>
2023-06-11 19:44:26 +0800

    replace links template

--- usr/themes/nexmoe/links.php
+++ usr/themes/nexmoe/links.php
@@ -1,123 +1,48 @@
-<?php
+<?php 
 /**
- * 友情链接
- *
- * @package custom
- *
- */
-if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-$this->need('layout/_partial/head.php');
-?>
-<style>
-    article ul li {
-        transition: all .03s;
-        width: calc(12.5% - 10px);
-        margin: 5px;
-        border-radius: 4px;
-        border: 1px solid #eee;
+* 友情链接 by onesrc.cn
+*
+* @package custom
+*/
+if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+<?php $this->need('header.php'); ?>
 
-        overflow: hidden;
-    }
-
-    article ul {
-        list-style-type: none;
-        padding: 0;
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        margin: -5px;
-    }
-
-    article ul li img {
-        height: 110px;
-        width: 100%;
-        border-radius: 0;
-        border: none;
-        display: block;
-    }
-
-    article ul li a {
-        top: 10px;
-        font-size: 15px;
-        color: #606266;
-        -webkit-transition: none;
-        transition: none;
-        margin-bottom: 5px;
-        display: block;
-        position: relative;
-        width: 100%;
-        border: none;
-        height: 30px;
-        margin-top: -2px;
-        text-align: center;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
-    }
-
-    @media screen and (max-width: 768px) {
-        article ul li {
-            width: calc(33.3333333333% - 10px);
-        }
-</style>
-<link rel="stylesheet" href="<?php echo $this->options->highlight_css ?>">
-<link rel="stylesheet" href="<?php echo $this->options->fancybox_css ?>">
-<body class="mdui-drawer-body-left">
-<?php $this->need('layout/_partial/background.php'); ?>
-<div id="nexmoe-header">
-    <?php $this->need('layout/_partial/header.php'); ?>
-</div>
-<div id="nexmoe-content">
-    <div class="nexmoe-primary">
-
-        <div class="nexmoe-post">
-
-            <article>
-                <?php
-                $str = preg_replace('#<li>(.*?)</li>#', '<li>$1</li>', $this->content);
-                $str = preg_replace('#<li></li>#', '<li>$1<a href="$2"><p>$3</p></a>', $str);
-                $str = preg_replace('#<ul>#', '<ul>', $str);
-                $str = preg_replace('#</ul>#', '</ul>', $str);
-
-                $str = preg_replace('#<a href="(.*?)">(.*?)</a>#', '<a href="$1"  target="_blank" >$2</a>', $str);
-                echo $str;
-                ?>
-            </article>
-            <div id="comments">
-                <?php $this->need('comments.php'); ?>
-            </div>
+<article class="main-content page-page">
+    <div class="post-header">
+        <h1 class="post-title" itemprop="name headline">
+            <?php $this->title() ?>
+        </h1>
+        <div class="post-data">
+            <time datetime="<?php $this->date('c'); ?>" itemprop="datePublished">Published on <?php $this->date('M j, Y'); ?></time>
         </div>
-
-
     </div>
-</div>
-<?php $this->need('layout/_partial/after-footer.php'); ?>
-<script src="<?php echo $this->options->jquery_js ?>"></script>
+    <div id="post-content" class="post-content">
+        <?php parseContent($this); ?>
+    </div>
+</article>
+<script>
+(function(){
+    let a =document.getElementById("flinks");
+    if(a){
+        let ns = a.querySelectorAll("li");
+        let nsl = ns.length;
+        let str ='<div class="post-lists"><div class="post-lists-body" id ="flinksH">';
+        let bgid = 0;
+        const bgs =["bg-white","bg-grey","bg-deepgrey","bg-blue","bg-purple","bg-green","bg-yellow","bg-red","bg-orange"];
+        for(let i = 0;i<=nsl-4;i+=4){
+           bgid = Math.floor(Math.random() * 9);
+            str += (`<div class="post-list-item"><div class="post-list-item-container "><div class="item-label ${bgs[bgid]}"><div class="item-title"><a href="${ns[i+1].innerText}">${ns[i].innerText}</a></div><div class="item-meta clearfix"><div class="item-meta-ico bg-ico-book"style="background: url(${ns[i+2].innerText}) no-repeat;background-size: 40px auto;"></div><div class="item-meta-date">${ns[i+3].innerText}</div></div></div></div></div>`);
+        }
+        str+='</div></div><style>.post-list-item{width: 50%;min-width: 300px;}</style>';
+        let n1 = document.createElement("div");
+        n1.innerHTML = str;
+        a.parentNode.insertBefore(n1,a);
+        a.style="display: none;";
+    }else{
+        console.log('No such id "flinksH"');
+    }
+}())
+ </script>
+<?php $this->need('comments.php'); ?>
 
-<?php if (!empty($this->options->function) && in_array('fancybox', $this->options->function)): ?>
-    <script src="<?php echo $this->options->fancybox_js ?>"></script>
-<?php endif; ?>
-<?php if (!empty($this->options->function) && in_array('enableMathjax', $this->options->function)): ?>
-    <?php if ($this->fields->math != "No"): ?>
-        <script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
-
-        </script>'
-        <script type="text/javascript"
-                src="https://cdn.bootcss.com/mathjax/2.7.6/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
-    <?php endif; ?>
-<?php endif; ?>
-<?php if (empty($this->options->function) || !in_array('enableMathjax', $this->options->function)): ?>
-    <?php if ($this->fields->math == "Yes"): ?>
-        <script type="text/x-mathjax-config">MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
-
-        </script>'
-        <script type="text/javascript"
-                src="https://cdn.bootcss.com/mathjax/2.7.6/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
-    <?php endif; ?>
-<?php endif; ?>
-
-<script src="<?php echo $this->options->highlight_js ?>"></script>
-<script>hljs.initHighlightingOnLoad();</script>
-</body>
-
-</html>
+<?php $this->need('footer.php'); ?>
