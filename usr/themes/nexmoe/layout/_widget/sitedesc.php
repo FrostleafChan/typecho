cc3253b7c808efef4ff7c3de0c8a5646f8842af3
Elvin Starry <ltdoge@ltdoge.top>
2023-06-11 14:40:29 +0800

    desc padding 10px

--- usr/themes/nexmoe/layout/_widget/sitedesc.php
+++ usr/themes/nexmoe/layout/_widget/sitedesc.php
@@ -1,6 +1,8 @@
 <div class="nexmoe-widget-wrap">
-    <h3 class="nexmoe-widget-title"><i class="mdui-icon material-icons">&#xe42d;</i>霜叶小屋</h3>
-    <div class="nexmoe-widget">
-        <p>“轻击键盘，静候回音。”</p>
+    <div class="widget-header">
+        <h3 class="nexmoe-widget-title"><i class="mdui-icon material-icons">&#xe42d;</i>霜叶小屋</h3>
+        <div class="widget-content">
+            <p style="padding: 10px">“轻击键盘，静候回音。”</p>
+        </div>
     </div>
 </div>
